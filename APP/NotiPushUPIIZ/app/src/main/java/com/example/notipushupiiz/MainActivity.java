23a5b44a744e155d1c9c1bf8807d016b8bcc5a88
Skabe79 <skabe79@gmail.com>
2020-12-20 16:58:51 -0600

    Primera version diseños

--- APP/NotiPushUPIIZ/app/src/main/java/com/example/notipushupiiz/MainActivity.java
+++ APP/NotiPushUPIIZ/app/src/main/java/com/example/notipushupiiz/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.notipushupiiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    boolean iniciado=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(!iniciado){
+            Intent intent= new Intent(getApplicationContext(),LoginInicial.class);
+            startActivity(intent);
+        }else{
+            
+        }
     }
 }
