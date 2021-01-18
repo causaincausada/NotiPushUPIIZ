a7f2cf988268a7bc640cd98b1fc7483882a6a312
CarlosZapata <czapatac1500@alumno.ipn.mx>
2021-01-18 13:05:49 -0600

    API notificacion

---
+++ WEB/php/notificacion.php
@@ -0,0 +1,95 @@
+<?php
+    $respuesta = array();
+    $respuesta['correcto']='0';
+    include('conexion.php');
+    switch ($_SERVER['REQUEST_METHOD']) {
+        case 'GET':
+            get_Notificacion();
+            break;
+        
+        case 'POST':
+            alta_Notificacion();
+            break;
+
+        default:
+            die;
+            break;
+    }
+
+    ///GET
+    function get_Notificacion()
+    {
+        if (isset($_GET['idNotificacion'])) {
+            $idNotificacion = $_GET['idNotificacion'];
+            $link=connect();
+            $consulta="SELECT * FROM `notificacion` WHERE `idNotificacion` = " . $idNotificacion;
+            $resultado = mysqli_query($link, $consulta) or error_Consulta();
+            mysqli_close($link);
+    
+            $notificaciones = array();
+            while ($notificacion = mysqli_fetch_assoc($resultado)) {
+                $notificaciones[] = $notificacion;
+            }
+    
+            $respuesta['notificaciones'] = array();
+            $respuesta['notificaciones'] = $notificaciones;
+            $respuesta['correcto'] = true;
+            echo json_encode($respuesta);
+            die;
+        }
+    
+        $link=connect();
+        $consulta="SELECT * FROM `notificacion`";
+        $resultado = mysqli_query($link, $consulta) or error_Consulta();
+        mysqli_close($link);
+    
+        $notificaciones = array();
+        while ($notificacion = mysqli_fetch_assoc($resultado)) {
+            $notificaciones[] = $notificacion;
+        }
+    
+        $respuesta['notificaciones'] = array();
+        $respuesta['notificaciones'] = $notificaciones;
+        $respuesta['correcto'] = true;
+        echo json_encode($respuesta);
+        die;
+    }
+    
+    ///POST
+    function alta_Notificacion()
+    {
+        $link=connect();
+        $respuesta['correcto']='0';
+
+        $d = file_get_contents("php://input");
+        $datos = json_decode($d);
+
+        if (empty($d)) {;
+            echo json_encode($respuesta);
+            die;
+        }
+
+        if (!isset($datos->titulo) || !isset($datos->descripcion) || !isset($datos->fecha) || !isset($datos->Grupo_idGrupo)) {
+            echo json_encode($respuesta);
+            die;
+        }
+
+        $titulo = $datos->titulo;
+        $descripcion = $datos->descripcion;
+        $fecha = $datos->fecha;
+        $Grupo_idGrupo = $datos->Grupo_idGrupo;
+
+        $sentencia="INSERT INTO `notificacion` (`idNotificacion`, `titulo`, `descripcion`, `fecha`, `Grupo_idGrupo`) VALUES (NULL, '".$titulo."', '".$descripcion."', '".$fecha."', '".$Grupo_idGrupo."')";
+        $BD= mysqli_query($link, $sentencia) or error_Consulta();
+        $respuesta['correcto']='1';
+        
+        echo json_encode($respuesta);
+    }
+
+    function error_Consulta()
+    {
+        $respuesta = array();
+        $respuesta['correcto']='0';
+        echo json_encode($respuesta);
+        die;
+    }
