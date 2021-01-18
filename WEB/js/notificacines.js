d36ba5faf13acbe66b9e8d0cbf56c15f3f01d502
CarlosZapata <czapatac1500@alumno.ipn.mx>
2021-01-18 14:30:58 -0600

    Acomadar cambios de la api

--- WEB/js/notificacines.js
+++ WEB/js/notificacines.js
@@ -1,84 +1,39 @@
-//Traer estos datos de la api
-/*var nameGrupos = [];
-nameGrupos.push("Hola");
-nameGrupos.push("Hola2");
-nameGrupos.push("Hola3");
-nameGrupos.push("Hola");
-nameGrupos.push("Hola2");
-nameGrupos.push("Hola3");
-
-var idGrupos = [];
-idGrupos.push("1");
-idGrupos.push("2");
-idGrupos.push("3");
-idGrupos.push("1");
-idGrupos.push("2");
-idGrupos.push("3");*/
-
 var dataSet = [
-  [ "Tiger Nixon", "System Architect", "Edinburgh" ],
-  [ "Garrett Winters", "Accountant", "Tokyo" ],
-  [ "Ashton Cox", "Junior Technical Author", "San Francisco"],
+  ["Tiger Nixon", "System Architect", "Edinburgh"],
+  ["Garrett Winters", "Accountant", "Tokyo"],
+  ["Ashton Cox", "Junior Technical Author", "San Francisco"],
 ];
 
 const xml = new XMLHttpRequest();
 xml.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     const respuesta = JSON.parse(xml.responseText);
-    console.log(respuesta);
-    if (respuesta.correcto == "1") {
+    if (respuesta.correcto) {
       setGrupos(respuesta.grupos);
     }
   }
 };
 
-xml.open("POST", "php/traer_grupos.php", true);
+xml.open("GET", "php/grupo.php", true);
 xml.send();
-
-var dataSet = [
-  [ "Tiger Nixon", "System Architect", "Edinburgh" ],
-  [ "Garrett Winters", "Accountant", "Tokyo" ],
-  [ "Ashton Cox", "Junior Technical Author", "San Francisco"],
-];
 
 const xml33 = new XMLHttpRequest();
 xml33.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     const respuesta = JSON.parse(xml33.responseText);
-    console.log(respuesta);
     if (respuesta.correcto == "1") {
-      setNoti(respuesta.Notificaciones);
+      setNoti(respuesta.notificaciones);
     }
   }
 };
-xml33.open("POST", "php/traer_notificaciones.php", true);
+xml33.open("GET", "php/notificacion.php", true);
 xml33.send();
 
-//Traer estos datos de la api
-/*var asuntos = [];
-asuntos.push("a");
-asuntos.push("a");
-asuntos.push("a");
-
-var grupo = [];
-grupo.push("x");
-grupo.push("x");
-grupo.push("x");
-
-var idNoti = [];
-grupo.push("1");
-grupo.push("2");
-grupo.push("3");
-
-
-
-setNoti(asuntos, grupo, idNoti);*/
-
 function setNoti(notificaciones) {
-  
-  for (var i = 0; i <notificaciones.length; i++){
-    var b = "<button type='button' class='btn btn-outline-primary' data-toggle='modal' data-target='#modal-VerNotificacion' style='width: 100%;'> VER</button>";
-    var d = [notificaciones[i].titulo, notificaciones[i].idNotificacion , b];
+  for (var i = 0; i < notificaciones.length; i++) {
+    var b =
+      "<button type='button' class='btn btn-outline-primary' data-toggle='modal' data-target='#modal-VerNotificacion' style='width: 100%;'> VER</button>";
+    var d = [notificaciones[i].titulo, notificaciones[i].idNotificacion, b];
     dataSet.push(d);
   }
 
@@ -120,7 +75,7 @@ document.getElementById("btn-enviar").addEventListener(
     }
 
     if (!error) {
-      const xml_nueva= new XMLHttpRequest();
+      const xml_nueva = new XMLHttpRequest();
       xml_nueva.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
           const respuesta = JSON.parse(xml_nueva.responseText);
@@ -131,47 +86,47 @@ document.getElementById("btn-enviar").addEventListener(
           }
         }
       };
-      xml_nueva.open("POST", "php/nueva_notificacion.php?titulo=hola&descripcion=holimunto&fecha=17072000&Grupo_idGrupo=1", true);
+      xml_nueva.open(
+        "POST",
+        "php/nueva_notificacion.php?titulo=hola&descripcion=holimunto&fecha=17072000&Grupo_idGrupo=1",
+        true
+      );
       xml_nueva.send();
     }
   },
   false
 );
 
-function setTablaData(){
+function setTablaData() {
   $("#example1")
-          .DataTable({
-            data: dataSet,
-      columns: [
-          { title: "Autor" },
-          { title: "Grupo" },
-          { title: "Acción" },
-      ],
-            language: {
-              decimal: "",
-              emptyTable: "No hay información",
-              info: "Mostrando _START_ a _END_ de _TOTAL_ Notificaciones",
-              infoEmpty: "Mostrando 0 to 0 of 0 Notificaciones",
-              infoFiltered: "(Filtrado de _MAX_ total Notificaciones)",
-              infoPostFix: "",
-              thousands: ",",
-              lengthMenu: "Mostrar _MENU_ Notificaciones",
-              loadingRecords: "Cargando...",
-              processing: "Procesando...",
-              search: "Buscar:",
-              zeroRecords: "Sin resultados encontrados",
-              paginate: {
-                first: "Primero",
-                last: "Ultimo",
-                next: "Siguiente",
-                previous: "Anterior",
-              },
-            },
-            responsive: true,
-            lengthChange: true,
-            autoWidth: false,
-          })
-          .buttons()
-          .container()
-          .appendTo("#example1_wrapper .col-md-6:eq(0)");
+    .DataTable({
+      data: dataSet,
+      columns: [{ title: "Autor" }, { title: "Grupo" }, { title: "Acción" }],
+      language: {
+        decimal: "",
+        emptyTable: "No hay información",
+        info: "Mostrando _START_ a _END_ de _TOTAL_ Notificaciones",
+        infoEmpty: "Mostrando 0 to 0 of 0 Notificaciones",
+        infoFiltered: "(Filtrado de _MAX_ total Notificaciones)",
+        infoPostFix: "",
+        thousands: ",",
+        lengthMenu: "Mostrar _MENU_ Notificaciones",
+        loadingRecords: "Cargando...",
+        processing: "Procesando...",
+        search: "Buscar:",
+        zeroRecords: "Sin resultados encontrados",
+        paginate: {
+          first: "Primero",
+          last: "Ultimo",
+          next: "Siguiente",
+          previous: "Anterior",
+        },
+      },
+      responsive: true,
+      lengthChange: true,
+      autoWidth: false,
+    })
+    .buttons()
+    .container()
+    .appendTo("#example1_wrapper .col-md-6:eq(0)");
 }
