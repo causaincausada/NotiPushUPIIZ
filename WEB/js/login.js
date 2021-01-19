b81df7129ba2100bcc159bc47a461155508a57e1
CarlosZapata <czapatac1500@alumno.ipn.mx>
2021-01-19 16:10:59 -0600

    login

--- WEB/js/login.js
+++ WEB/js/login.js
@@ -1,15 +1,31 @@
 window.onload = function () {
-    document.getElementById("form_login").addEventListener(
-      "submit",
-      function (event) {
-        event.preventDefault();
+  document.getElementById("form_login").addEventListener(
+    "submit",
+    function (event) {
+      event.preventDefault();
 
-        console.log("peticion a la api");
+      var usuario = document.getElementById("username").value;
+      var clave = document.getElementById("password").value;
 
-
-        location.href = "Menu.html";
-        sessionStorage.setItem("tipo", "1");
-      },
-      false
-    );
-  };
+      const xml1 = new XMLHttpRequest();
+      xml1.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          const respuesta = JSON.parse(xml1.responseText);
+          if (respuesta.correcto) {
+            if (respuesta.login) {
+              location.href = "Menu.html";
+              sessionStorage.setItem("tipo", "1");
+            } else {
+              alert("Usuario o contraseña incorrectos ");
+              document.getElementById("username").value = "";
+              document.getElementById("password").value = "";
+            }
+          }
+        }
+      };
+      xml1.open("GET", "php/login.php?usuario=" + usuario +"&clave=" + clave, true);
+      xml1.send();
+    },
+    false
+  );
+};
